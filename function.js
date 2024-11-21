f8f40377ded5335e9ab1857e3f70c0c5029903b0
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-22 03:52:45 +0600

    function call

---
+++ function.js
@@ -0,0 +1,9 @@
+// declaration function
+function mobile(){
+    console.log('Name:','Iphone')
+    console.log('Price:',25000);
+    console.log('Stroge','256gb');
+}
+
+// call the function
+mobile();
