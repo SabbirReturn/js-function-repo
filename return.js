a9506eb467f28528512f262b2d922560d90d73e3
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-26 16:25:03 +0600

    return

--- return.js
+++ return.js
@@ -1,6 +1,16 @@
-function tenTime(number){
-    const result = number * 10;
-    return result
+// function tenTime(number){
+//     const result = number * 10;
+//     return result
+// }
+// const outPut = tenTime(9)
+// console.log(outPut)
+
+function even(number){
+    for(let i=0;i<number; i++){
+        let num = console.log(i)
+        return num
+        
+    }
 }
-const outPut = tenTime(9)
+let outPut = even(100);
 console.log(outPut)
