973669bcc5020caeb854bf1fc3d61c0f7d491a88
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-29 04:59:22 +0600

    diffrent pra

--- different_pra.js
+++ different_pra.js
@@ -11,3 +11,18 @@ function evenSizeString (str){
 // let output1 =evenSizeString('Dhaka');
 // let output2=evenSizeString('faka')
 // console.log(output1,output2);
+
+function doOrdouble (num, doOrdouble){
+    if(doOrdouble===true){
+        let result = num * 3;
+        return result;
+    }
+    else{
+        let result = num * 2;
+        return result
+    }
+}
+let output1 = doOrdouble(5, true);
+let output2 = doOrdouble(5, false);
+console.log(output1);
+console.log(output2);
