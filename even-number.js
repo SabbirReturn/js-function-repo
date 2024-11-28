287b64675b429c4c111bc19ea93737948fc4b622
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-11-29 05:47:18 +0600

    even-number

--- even-number.js
+++ even-number.js
@@ -1,14 +1,28 @@
-function evenNumber(numbers){
-    let evens = [];
+// function evenNumber(numbers){
+//     let evens = [];
+//     for(let number of numbers){
+//         // console.log(number);
+//         if(number % 2===0){
+//             // console.log(number);
+//             evens.push(number);;
+//         }
+//     }
+//     return evens;
+// }
+// let numbs = [1,2,3,4,5,6,7,8,9,10];
+// let num = evenNumber(numbs);
+// console.log(num)
+
+function sumOfevenNumber(numbers){
+    let sum =0;
     for(let number of numbers){
         // console.log(number);
         if(number % 2===0){
-            // console.log(number);
-            evens.push(number);;
+            sum = sum + number;
         }
     }
-    return evens;
+    return sum;
 }
 let numbs = [1,2,3,4,5,6,7,8,9,10];
-let num = evenNumber(numbs);
-console.log(num)
+let num = sumOfevenNumber(numbs);
+console.log('sum of even number', num);
