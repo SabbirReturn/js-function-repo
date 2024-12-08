0f3d3e9602c7321862fc1e29ec4cb00059d58fa2
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-12-09 02:18:42 +0600

    recheak

--- leap_year.js
+++ leap_year.js
@@ -6,5 +6,16 @@ function leapyear(year){
         return false;
     }
 }
-let lyear = leapyear(2021);
-console.log(lyear);
+// let lyear = leapyear(2021);
+// console.log(lyear);
+
+function isLeapYear(year){
+    if(year % 100 !==0 & year % 4===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+let leapyear1= isLeapYear(2024);
+console.log(leapyear1);
