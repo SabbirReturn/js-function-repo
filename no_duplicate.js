4e4e6aa610a1eb9983e3f9090d07210ea46d6359
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-12-09 03:14:17 +0600

    no_duplicate

---
+++ no_duplicate.js
@@ -0,0 +1,12 @@
+function noDuplicate(array){
+    let uniquePerson = [];
+    for(let iteam of array){
+        if(uniquePerson.includes(iteam) ===false){
+            uniquePerson.push(iteam);
+        }
+    }
+    return uniquePerson
+}
+let berinyKhor = ['Sabbir','Hasan','Robin','Misu','Sabbir','Hasan'];
+let unique = noDuplicate(berinyKhor);
+console.log(unique);
