dab70bf703897837db89e5d79d604c22d2191155
SabbirReturn <mdsabbirhasan39@gmail.com>
2024-12-10 02:23:33 +0600

    math.js

---
+++ math.js
@@ -0,0 +1,5 @@
+// let numbers= [12,13,112,11,13,15,2000,113];
+let minimumNumber = Math.min(12,13,112,11,13,15,2000,113);
+let maximumNUmber = Math.max(12,13,112,11,13,15,2000,113);
+console.log(minimumNumber);
+console.log(maximumNUmber);
