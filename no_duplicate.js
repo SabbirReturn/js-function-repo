f1d8762f6c828d3d59c0d72a88add2d796550dd4
Sabbir Hasan <123839194+SabbirReturn@users.noreply.github.com>
2025-04-06 22:30:55 +0600

    Update no_duplicate.js

--- no_duplicate.js
+++ no_duplicate.js
@@ -10,3 +10,18 @@ function noDuplicate(array){
 let berinyKhor = ['Sabbir','Hasan','Robin','Misu','Sabbir','Hasan'];
 let unique = noDuplicate(berinyKhor);
 console.log(unique);
+
+
+
+function noDuplicate(array){
+    let uniquePerson = [];
+    for(let iteam of array){
+        if(uniquePerson.includes(iteam) ===false){
+            uniquePerson.push(iteam);
+        }
+    }
+    return uniquePerson
+}
+let berinyKhor = ['Sabbir','Hasan','Robin','Misu','Sabbir','Hasan'];
+let unique = noDuplicate(berinyKhor);
+console.log(unique);
